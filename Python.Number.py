4fcb37619371e2a507470ebf3e2c7c1528e06724
unknown <minmin.minnaing7@gmail.com>
2019-12-21 00:21:57 +0630

    1st commit

---
+++ Python.Number.py
@@ -0,0 +1,41 @@
+Python 3.7.5 (tags/v3.7.5:5c02a39a0b, Oct 15 2019, 00:11:34) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello World")
+Hello World
+>>> 2+2
+4
+>>> 50-5*6
+20
+>>> (50-5*6)/4
+5.0
+>>> 8?
+SyntaxError: invalid syntax
+>>> 8/5
+1.6
+>>> 5**2
+25
+>>> 2**7
+128
+>>> width=20
+>>> height=5*9
+>>> width * height
+900
+>>> 'spam eggs'
+'spam eggs'
+>>> 'doesn\'t'
+"doesn't"
+>>> "doesn't"
+"doesn't"
+>>> '"Yes,"they said.'
+'"Yes,"they said.'
+>>> "\"Yes,\"they said."
+'"Yes,"they said.'
+>>> "\"Isn't,\"they said."
+'"Isn\'t,"they said.'
+>>> print("Min Min.\nThet Htar Yee Maung.\nWai Yan Linn.\nAung Myat.\nThan Htike Zaw.
+      
+SyntaxError: EOL while scanning string literal
+>>> print("Min Min\nThet Htar Yee Maung")
+Min Min
+Thet Htar Yee Maung
+>>> 
